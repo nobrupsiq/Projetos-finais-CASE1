553ae4ce25fd74c72fbb03c049b7a0ae2b75ca52
bruno <bpires.siqueira@gmail.com>
2023-02-23 00:07:54 -0300

    update server.js

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,13 +1,13 @@
-import app from './app.js'
-import db from './infra/db.js'
+import app from "./app.js";
+import db from "./infra/db.js";
 
 // escolhendo a porta em que o servidor será aberto
-const port = 3000
-import './infra/contents.js'
+const port = 3000;
+import "./infra/contents.js";
 
 // abrindo o servidor na porta escolhida
-app.listen(port,  ()=>{
-    db.run(`delete from conteudos`)
+app.listen(port, () => {
+  db.run(`delete from produtos`);
 
-    console.log(`Server rodando em http://localhost:${port}/`)
-})
+  console.log(`Server rodando em http://localhost:${port}/`);
+});
